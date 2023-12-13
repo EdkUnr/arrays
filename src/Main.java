1fc34c525b071477f61cf5d7af6cc2b2b9e94348
EdkUnr <richardlvov1780@gmail.com>
2023-12-13 13:06:10 +1000

    Homework "Arrays2"

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,61 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1(); //Задача 1
+        task2(); //Задача 2
+        task3(); //Задача 3
+        correctFullNameTask(); //Задача 4
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
         }
+        return arr;
+    }
+
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        int total = 0;
+        for (int payment : arr) {
+            total += payment;
+        }
+        System.out.println("Сумма трат за месяц составила " + total + " рублей");
+    }
+
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        int min = arr[0];
+        int max = arr[0];
+        for (int payment : arr) {
+            if (payment < min) {
+                min = payment;
+            }
+            if (payment > max) {
+                max = payment;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей.");
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей.");
+    }
+
+    public static void task3() {
+        int[] arr = generateRandomArray();
+        double total = 0;
+        for (int payment : arr) {
+            total += payment;
+        }
+        double average = total / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
+    }
+
+    public static void correctFullNameTask() {
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        System.out.print("Исправленное полное имя: ");
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
     }
 }
